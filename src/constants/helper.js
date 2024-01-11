70f50f4fa7fb9aed1147ebf7de62d3b2fc861a58
Prince Raghuvanshi <adityaraghuvanshi159@gmail.com>
2024-01-11 17:12:28 +0530

    added all the files

---
+++ src/constants/helper.js
@@ -0,0 +1,49 @@
+export const questions = [
+    {
+      text: "The synchronous modems are more costly than the asynchronous modems because ?",
+      options: [
+        { id: 0, text: "they produce large volume of data", isCorrect: false },
+        { id: 1, text: "they contain clock recovery circuits ", isCorrect: true },
+        { id: 2, text: "they transmit the data with stop and start bits. ", isCorrect: false },
+        { id: 3, text: "they operate with a larger bandwidth ", isCorrect: false },
+      ],
+    },
+    {
+      text: "Telnet is a ______ based computer protocol ?",
+      options: [
+        { id: 0, text: "Sound ", isCorrect: false },
+        { id: 1, text: "Text", isCorrect: true },
+        { id: 2, text: "image", isCorrect: false },
+        { id: 3, text: "Animation", isCorrect: false },
+      ],
+    },
+    {
+      text: "	Computers that control processes accept data in a continuous __________ ?",
+      options: [
+        { id: 0, text: "data traffic pattern ", isCorrect: false },
+        { id: 1, text: "Data highway", isCorrect: false },
+        { id: 2, text: "Infinity loop", isCorrect: false },
+        { id: 3, text: "feedback loop ", isCorrect: true },
+      ],
+    },
+    {
+      text: "Computers connected to a LAN can ?",
+      options: [
+        { id: 0, text: "run faster ", isCorrect: false },
+        { id: 1, text: "go on line", isCorrect: false },
+        { id: 2, text: "share information and/or share peripheral equipment ", isCorrect: true },
+        { id: 3, text: "E-mail ", isCorrect: false },
+      ],
+    },
+    {
+      text: "Computer and communication technology, such as communication links to the Internet, that provide help and understanding to the end user is known as ?",
+      options: [
+        { id: 0, text: "presentation file ", isCorrect: false },
+        { id: 1, text: "Information Technology", isCorrect: true },
+        { id: 2, text: "program", isCorrect: true },
+        { id: 3, text: "worksheet file", isCorrect: false },
+      ],
+    },
+  ];
+
+  
